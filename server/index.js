7ab3cf6b9004622ddddb4294e7fcba052d7b886f
Puneethkumar <puneethkumarg96@gmail.com>
2024-01-27 11:54:09 +0530

    Server Setup and created API fro authentication

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const http  = require("http");
+const authRouter  = require('./routes/auth');
+const mongoose = require("mongoose");
 
-const port = 3000;
+const port = 4000;
 const app = express();
+const DB = "mongodb+srv://puneeth:puni123@cluster0.imdw0te.mongodb.net/?retryWrites=true&w=majority";
+
+
+app.use(express.json());
+app.use(authRouter);
+
+mongoose
+.connect(DB)
+.then(()=>{
+    console.log("Database connection is successfull");
+}).catch((e)=>{
+    console.log(e);
+})
 
 
 
-app.listen(port,'0.0.0.0',()=>{
+app.listen(port,()=>{
     console.log("Waiting for connection");
 });
---
+++ server/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userScheme = mongoose.Schema({
+    name:{
+        required : true,
+        type :String,
+        trim:true,
+    },
+    password:{
+        required:true,
+        type: String,
+    },
+    email:{
+        required:true,
+        type:String,
+        trim:true,
+        validate:{
+            validator:(value) => {
+                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                return value.match(re);
+            },
+            message: "please enter the valid email",
+        }
+    },
+    address:{
+        type:String,
+        deafault:"",
+    },
+    type:{
+        type:String,
+        default:'user',
+    }
+});
+
+const User = mongoose.model("User",userScheme);
+
+module.exports = User;
